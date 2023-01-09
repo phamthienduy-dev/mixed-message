314124e2bcb4738ba05de8d1178a6219451538cf
Phạm Thiên Duy <duyphamthien@outlook.com>
2023-01-09 11:40:30 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mix Message - Codecademy project
+
+## Usage
+
+Generate a random quote when you run the script.
---
+++ script.js
@@ -0,0 +1,12 @@
+const messages = [
+  "The purpose of our lives is to be happy.",
+  "Life is what happens when you're busy making other plans.",
+  "Get busy living or get busy dying.",
+  "You only live once, but if you do it right, once is enough.",
+  "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+  "If you want to live a happy life, tie it to a goal, not to people or things.",
+  "Never let the fear of striking out keep you from playing the game.",
+  "Money and success don’t change people; they merely amplify what is already there.",
+];
+
+const randomIndex = Math.floor(Math.random() * messages.length);
